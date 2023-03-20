458694924b9aed329427722ec9926a34c74698bd
Matthias Clein <m.cleinespinoza@gmail.com>
2023-03-20 18:07:35 -0300

    add search by last name that retrieve A1 data

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ import streamlit as st
 import sqlite3
 
 def main():
-    pass
 
+    con = sqlite3.connect("./data/lific.db")
+    cur = con.cursor()
+
+    apellido_1 = st.text_input('Apellido 1').upper()
+    apellido_2 = st.text_input('Apellido 2').upper()
+
+    st.title(f"{apellido_1} {apellido_2}")
+    
+    res = cur.execute(f"SELECT nombres, apellido_1, apellido_2, a1.asignatura, año, semestre, promedio_final, estado FROM estudiantes INNER JOIN a1 ON estudiantes.matricula = a1.matricula WHERE apellido_1 = '{apellido_1}' AND apellido_2 = '{apellido_2}'").fetchall()
+
+    st.dataframe(data = res)
 if __name__ == "__main__":
     main()
